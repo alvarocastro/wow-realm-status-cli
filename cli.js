9c40e2d1a7e0206a29c34515cb970fa88364e694
Alvaro Castro <moltar89@gmail.com>
2019-09-11 14:08:53 -0300

    Reorganize code

--- cli.js
+++ cli.js
@@ -4,50 +4,14 @@ const fs = require('fs');
 const program = require('commander');
 const ora = require('ora');
 const {fetchRealm} = require('wow-realm-status');
-const placeholdify = require('placeholdify');
+const {
+	normalizeRealm,
+	filterFields,
+	outputAsJson,
+	outputAsHuman
+} = require('.');
 
 const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-
-const normalizeRealm = function (data) {
-	return {
-		name: data.name,
-		locale: data.locale,
-		timezone: data.timezone,
-		status: data.online ? 'online' : 'offline',
-		type: data.type,
-		population: data.population
-	};
-};
-
-const filterFields = function (data, fields = []) {
-	return fields.reduce((out, field) => {
-		out[field] = data[field];
-		return out;
-	}, {});
-};
-
-const outputAsJson = function (data) {
-	console.log(JSON.stringify(data));
-};
-
-const outputAsHuman = function (data) {
-	const labels = {
-		name: 'Name: {0}',
-		status: 'Status: {0}',
-		population: 'Population: {0}',
-		type: 'Type: {0}',
-		locale: 'Locale: {0}',
-		timezone: 'Timezone: {0}'
-	};
-
-	for (const key of Object.keys(labels)) {
-		if (data[key] === undefined) {
-			continue;
-		}
-
-		console.log(placeholdify(labels[key], data[key]));
-	}
-};
 
 program
 	.version(pkg.version, '-v, --version', 'Output the current version')
@@ -83,7 +47,7 @@ program
 			}
 		} catch (error) {
 			spinner.stop();
-			console.log(`DIE INSECT! ${error.message}`);
+			console.error(`DIE INSECT! ${error.message}!`);
 		}
 	});
 
---
+++ index.js
@@ -0,0 +1,49 @@
+const placeholdify = require('placeholdify');
+
+const normalizeRealm = function (data) {
+	return {
+		name: data.name,
+		locale: data.locale,
+		timezone: data.timezone,
+		status: data.online ? 'online' : 'offline',
+		type: data.type,
+		population: data.population
+	};
+};
+
+const filterFields = function (data, fields = []) {
+	return fields.reduce((out, field) => {
+		out[field] = data[field];
+		return out;
+	}, {});
+};
+
+const outputAsJson = function (data) {
+	console.log(JSON.stringify(data));
+};
+
+const outputAsHuman = function (data) {
+	const labels = {
+		name: 'Name: {0}',
+		status: 'Status: {0}',
+		population: 'Population: {0}',
+		type: 'Type: {0}',
+		locale: 'Locale: {0}',
+		timezone: 'Timezone: {0}'
+	};
+
+	for (const key of Object.keys(labels)) {
+		if (data[key] === undefined) {
+			continue;
+		}
+
+		console.log(placeholdify(labels[key], data[key]));
+	}
+};
+
+module.exports = {
+	normalizeRealm,
+	filterFields,
+	outputAsJson,
+	outputAsHuman
+};
